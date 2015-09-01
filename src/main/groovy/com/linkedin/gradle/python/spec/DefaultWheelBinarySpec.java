f4e1be1c3bae442c0815d8f77110095bb600da70
Ethan Hall <ethan@ehdev.io>
2015-08-31 20:41:43 -0700

    more things

--- src/main/groovy/com/linkedin/gradle/python/spec/DefaultWheelBinarySpec.java
+++ src/main/groovy/com/linkedin/gradle/python/spec/DefaultWheelBinarySpec.java
@@ -6,10 +6,6 @@ public class DefaultWheelBinarySpec extends BaseBinarySpec implements WheelBinar
 
     @Override
     protected String getTypeName() {
-        StringBuilder sb = new StringBuilder();
-        for(int i = str.length(); i > 0; i--) {
-            sb.append(str.chatAt(i - 1));
-        }
         return "Wheel";
     }
 

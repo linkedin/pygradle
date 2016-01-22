fd4268cdc54849b08a174a89a19383e8bf25bbff
Ethan Hall <ethan@ehdev.io>
2016-01-22 08:01:28 -0800

    Fixed test

--- src/test/groovy/com/linkedin/gradle/python/internal/DefaultPythonSourceSetTest.groovy
+++ src/test/groovy/com/linkedin/gradle/python/internal/DefaultPythonSourceSetTest.groovy
@@ -1,8 +1,8 @@
 package com.linkedin.gradle.python.internal
 
 
+import com.linkedin.gradle.python.PythonSourceSet
 import org.gradle.api.internal.file.FileResolver
-import org.gradle.internal.reflect.DirectInstantiator
 import org.gradle.language.base.sources.BaseLanguageSourceSet
 import spock.lang.Specification
 
@@ -11,7 +11,7 @@ class DefaultPythonSourceSetTest extends Specification {
     setup:
     def parent = "main"
     def fileResolver = Mock(FileResolver)
-    def sourceSet = BaseLanguageSourceSet.create(DefaultPythonSourceSet, "python", parent, fileResolver, DirectInstantiator.INSTANCE)
+    def sourceSet = BaseLanguageSourceSet.create(PythonSourceSet, DefaultPythonSourceSet, "python", parent, fileResolver)
 
     expect:
     sourceSet.displayName == "Python source 'main:python'"

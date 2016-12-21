a6a03cb4e6f6a39be8de5688ac7e9a51ee6710a4
Willie Slepecki <willie.slepecki@onuspride.com>
2016-12-20 22:23:55 -0500

    Working on gettting the tests to run again

--- pygradle-plugin/src/main/groovy/com/linkedin/gradle/python/util/SystemExecutables.java
+++ pygradle-plugin/src/main/groovy/com/linkedin/gradle/python/util/SystemExecutables.java
@@ -6,16 +6,19 @@ import org.apache.tools.ant.taskdefs.condition.Os;
  * Enum that allows for platform independent resolution of executables
  */
 public enum SystemExecutables {
-    PYTHON("python", "exe"),
-    PYTHON_VERSION_AWARE("python%s", "exe"),
-    ACTIVATE("activate", "bat");
+    PYTHON("python", "exe", null),
+    PYTHON_VERSION_AWARE("python%s", "exe", null),
+    ACTIVATE("activate", "bat", null),
+    TEST_FILE("example", "bat", "sh");
 
     private String exeName;
     private String winExtension;
+    private String nixExtension;
 
-    SystemExecutables(String exeName, String winExtension) {
+    SystemExecutables(String exeName, String winExtension, String nixExtension) {
         this.exeName = exeName;
         this.winExtension = winExtension;
+        this.nixExtension = nixExtension;
     }
 
     public String getExecutable() {
@@ -24,6 +27,12 @@ public enum SystemExecutables {
 
         if (Os.isFamily(Os.FAMILY_WINDOWS)) {
             exeNameReturn = exeName + "." +  winExtension;
+        } else if (Os.isFamily(Os.FAMILY_UNIX)) {
+            if (nixExtension != null) {
+                exeNameReturn = exeName + "." + nixExtension;
+            } else {
+                exeNameReturn = exeName;
+            }
         }
 
         return exeNameReturn;

79780e0746af56c7bbff32512e143a030e2f8667
Barry Warsaw <barry@python.org>
2017-07-10 15:34:39 -0400

    Refactor for better testing integration.
    * All the work of Python version checking is moved to a new method
      normalizePythonVersion() which does *not* also try to find the Python
      interpreter on disk.  That's because the latter fails in the normal Gradle
      testing environment.
    * Add the ability to change the mapping of default Python versions to specific
      Python versions.  E.g. normalizePythonVersion('2') is configurable.
    * Add some debugging output which will need to get stripped on merge.

--- pygradle-plugin/src/test/groovy/com/linkedin/gradle/python/extension/PythonDetailsTest.groovy
+++ pygradle-plugin/src/test/groovy/com/linkedin/gradle/python/extension/PythonDetailsTest.groovy
@@ -23,39 +23,108 @@ class PythonDetailsTest extends Specification {
 
     def project = new ProjectBuilder().build()
     def details = new PythonDetails(project)
-    def savedWhitelistedPythonVersions = PythonVersion.whitelistedPythonVersions
+    def originalWhitelistedPythonVersions = PythonVersion.whitelistedPythonVersions
+    def originalDefaultPython2 = PythonVersion.defaultPython2
+    def originalDefaultPython3 = PythonVersion.defaultPython3
 
     def cleanup() {
-        PythonVersion.whitelistedPythonVersions = savedWhitelistedPythonVersions
+        PythonVersion.whitelistedPythonVersions = originalWhitelistedPythonVersions
+        PythonVersion.defaultPython2 = originalDefaultPython2
+        PythonVersion.defaultPython3 = originalDefaultPython3
     }
 
-    def 'test set to acceptable version'() {
+    def 'test normalize to acceptable version 3.x'() {
         expect:
-        details.setPythonVersion('3.5')
+        details.normalizePythonVersion('3.5') == '3.5'
     }
 
-    def 'test set to unacceptable version'() {
+    def 'test normalize to acceptable version 2.x'() {
+        expect:
+        details.normalizePythonVersion('2.7') == '2.7'
+    }
+
+    def 'test normalize to default version 3'() {
+        expect:
+        details.normalizePythonVersion('3') == '3.5'
+    }
+
+    def 'test normalize to default version 2'() {
+        expect:
+        details.normalizePythonVersion('2') == '2.6'
+    }
+
+    def 'test normalize to new default version 3'() {
+        setup:
+        PythonVersion.defaultPython3 = '3.6'
+
+        expect:
+        details.normalizePythonVersion('3') == '3.6'
+    }
+
+    def 'test normalize to new default version 2'() {
+        setup:
+        PythonVersion.defaultPython2 = '2.7'
+
+        expect:
+        details.normalizePythonVersion('2') == '2.7'
+    }
+
+    def 'test normalize to unacceptable version 2.x'() {
         when:
-        details.setPythonVersion('2.5')
+        details.normalizePythonVersion('2.5')
 
         then:
         thrown(GradleException)
     }
 
-    def 'test set to customized acceptable version'() {
+    def 'test normalize to unacceptable version 3.x'() {
+        when:
+        details.normalizePythonVersion('3.2')
+
+        then:
+        thrown(GradleException)
+    }
+
+    def 'test normalize to acceptable customized version'() {
         setup:
         PythonVersion.whitelistedPythonVersions = [ '2.7', '3.5', '3.6' ]
 
         expect:
-        details.setPythonVersion('3.5')
+        details.normalizePythonVersion('3.5') == '3.5'
     }
 
-    def 'test set to customized unacceptable version'() {
+    def 'test normalize to unacceptable customized version'() {
         setup:
         PythonVersion.whitelistedPythonVersions = [ '2.7', '3.5', '3.6' ]
 
         when:
-        details.setPythonVersion('2.6')
+        details.normalizePythonVersion('2.6')
+
+        then:
+        thrown(GradleException)
+    }
+
+    // Most of the work is done by normalizePythonVersion() so we just add
+    // these two additional tests to prove that setting the Python version
+    // runs through the expected normalization gauntlet.
+
+    // XXX This fails because it tries to find the Python 3.5 interpreter,
+    // which doesn't appear to exist in the test environment:
+    //
+    // com.linkedin.gradle.python.extension.PythonDetailsTest > test set to acceptable version FAILED
+    //     com.linkedin.gradle.python.exception.MissingInterpreterException: Unable to find or execute python
+    //         at com.linkedin.gradle.python.extension.PythonDetails.updateFromPythonInterpreter(PythonDetails.java:59)
+    //         at com.linkedin.gradle.python.extension.PythonDetails.setPythonVersion(PythonDetails.java:142)
+    //         at com.linkedin.gradle.python.extension.PythonDetailsTest.test set to acceptable version(PythonDetailsTest.groovy:113)
+
+    // def 'test set to acceptable version'() {
+    //     expect:
+    //     details.setPythonVersion('3.5')
+    // }
+
+    def 'test set to unacceptable version'() {
+        when:
+        details.setPythonVersion('2.5')
 
         then:
         thrown(GradleException)

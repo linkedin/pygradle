404f5c358a69a03bec3f2ee6e527e798b23ec761
Andrei Efimov <andrei_efimov@epam.com>
2020-03-05 12:14:03 -0800

    Ivy and PyPi module, file names compatibility

--- pivy-importer/src/main/groovy/com/linkedin/python/importer/deps/DependencyDownloader.groovy
+++ pivy-importer/src/main/groovy/com/linkedin/python/importer/deps/DependencyDownloader.groovy
@@ -58,10 +58,7 @@ abstract class DependencyDownloader {
     abstract downloadDependency(
         String dep, boolean latestVersions, boolean allowPreReleases, boolean fetchExtras, boolean lenient)
 
-    protected static File downloadArtifact(File destDir, String url) {
-
-        def filename = FilenameUtils.getName(new URL(url).getPath())
-        def contents = new File(destDir, filename)
+    protected static File downloadArtifact(File contents, String url) {
 
         if (!contents.exists()) {
             def proxy = ProxyDetector.maybeGetHttpProxy()
@@ -88,12 +85,27 @@ abstract class DependencyDownloader {
     }
 
     /**
-     * Get the actual module name from artifact name, which has the correct letter case.
+     * Get the module name from artifact name, which has the correct letter case.
      * @param filename the filename of artifact
      * @param revision module version
-     * @return actual module name, which is from PyPI
+     * @return module name
      */
-    static String getActualModuleNameFromFilename(String filename, String revision) {
+    static String getModuleNameFromFilename(String filename, String revision) {
         return filename.substring(0, filename.indexOf(revision) - 1)
+    }
+
+    /**
+     * Compose filename from artifact file name and module name.
+     * Makes filename that has to be compatible with ivy layout pattern <code>[module]-[revision].[ext]</code>,
+     * where <code>[module]</code> is from PyPI metadata.
+     * @param name module name from PyPI metadata
+     * @param filename the filename of artifact
+     * @param revision module version
+     * @return
+     */
+    static String buildFilenameByModuleName(String moduleName, String filename, String revision) {
+        final String moduleNameFromFilename = getModuleNameFromFilename(filename, revision)
+
+        return filename.replace(moduleNameFromFilename, moduleName)
     }
 }
